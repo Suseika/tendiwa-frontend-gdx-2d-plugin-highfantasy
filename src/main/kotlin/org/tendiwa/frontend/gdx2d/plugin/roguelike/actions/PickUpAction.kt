dd2fa8a6ac71a7b5d0ce7432c8784676fa5767de
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-26 18:37:30 +0500

    Implement multilevel storage of Space data. For #T-21

--- src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/actions/PickUpAction.kt
+++ src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/actions/PickUpAction.kt
@@ -6,7 +6,6 @@ import org.tendiwa.backend.modules.roguelike.archetypes.Item
 import org.tendiwa.backend.modules.roguelike.playerVolition.pickUp
 import org.tendiwa.backend.space.aspects.Position
 import org.tendiwa.backend.space.chunks.chunkWithTile
-import org.tendiwa.backend.space.realThing.realThings
 import org.tendiwa.frontend.gdx2d.TendiwaGame
 import org.tendiwa.plane.grid.tiles.move
 
@@ -21,6 +20,7 @@ class PickUpAction(
     override fun invoke(dx: Int, dy: Int) {
         val pickUpTile = playerCharacter.aspect<Position>().tile.move(dx, dy)
         val item = game.reality.space.realThings
+            .planeAtZ(playerCharacter.aspect<Position>().voxel.z)
             .chunkWithTile(pickUpTile)
             .things
             .filter { it.aspect<Position>().tile == pickUpTile && it is Item }
--- src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/reactions/PlayerVisionChangeReaction.kt
+++ src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/reactions/PlayerVisionChangeReaction.kt
@@ -3,10 +3,8 @@ package org.tendiwa.frontend.gdx2d.plugin.roguelike.reactions
 import org.tendiwa.backend.existence.aspect
 import org.tendiwa.backend.modules.roguelike.aspects.PlayerVision
 import org.tendiwa.backend.space.aspects.Position
-import org.tendiwa.backend.space.realThing.realThings
 import org.tendiwa.backend.space.realThing.viewOfArea
 import org.tendiwa.backend.space.walls.hasWallAt
-import org.tendiwa.backend.space.walls.walls
 import org.tendiwa.frontend.gdx2d.GameReaction
 import org.tendiwa.frontend.gdx2d.TendiwaGame
 import org.tendiwa.frontend.gdx2d.plugin.roguelike.updateFieldOfView
@@ -29,20 +27,23 @@ internal class PlayerVisionChangeReaction(
     }
 
     private fun showSeenWalls(difference: PlayerVision.VisionDifference) {
+        val plane = game.reality.space.walls.planeAtZ(game.vicinity.boundsDepth)
         difference.seen
-            .filter { game.reality.space.walls.hasWallAt(it) }
+            .filter { plane.hasWallAt(it) }
             .forEach { game.gridActorRegistry.spawnWall(it) }
     }
 
     private fun destroyUnseenWalls(difference: PlayerVision.VisionDifference) {
+        val plane = game.reality.space.walls.planeAtZ(game.vicinity.boundsDepth)
         difference.unseen
-            .filter { game.reality.space.walls.hasWallAt(it) }
+            .filter { plane.hasWallAt (it) }
             .forEach { game.gridActorRegistry.removeWallActor(it) }
     }
 
     private fun showSeenThings(difference: PlayerVision.VisionDifference) {
         game.apply {
             reality.space.realThings
+                .planeAtZ(vicinity.boundsDepth)
                 .viewOfArea(vicinity.tileBounds)
                 .things
                 .filter {
@@ -55,6 +56,7 @@ internal class PlayerVisionChangeReaction(
 
     private fun destroyUnseenThings(difference: PlayerVision.VisionDifference) {
         game.reality.space.realThings
+            .planeAtZ(game.vicinity.boundsDepth)
             .viewOfArea(game.vicinity.tileBounds)
             .things
             .filter { thing ->
