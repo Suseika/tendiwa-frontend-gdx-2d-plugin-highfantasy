1e497726d8288d845002eb818f29d19108b64147
Georgy Vlasov <wlasowegor@gmail.com>
2016-04-02 07:40:45 +0500

    Handle the case when a RealThing moves into or out of view and has to be displayed or removed

--- src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/reactions/RealThingMovementReaction.kt
+++ src/main/kotlin/org/tendiwa/frontend/gdx2d/plugin/roguelike/reactions/RealThingMovementReaction.kt
@@ -2,6 +2,7 @@ package org.tendiwa.frontend.gdx2d.plugin.roguelike.reactions
 
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction
+import org.tendiwa.backend.space.Voxel
 import org.tendiwa.backend.space.aspects.Position
 import org.tendiwa.frontend.gdx2d.GameReaction
 import org.tendiwa.frontend.gdx2d.TendiwaGame
@@ -11,18 +12,41 @@ class RealThingMovementReaction(
 ) : GameReaction<Position.Change>(game) {
 
     override fun invoke(stimulus: Position.Change, done: () -> Unit) {
-        game.gridActorRegistry
-            .actorOf(stimulus.host)
-            ?.addAction(
-                Actions.sequence(
-                    MoveToAction().apply {
-                        x = stimulus.new.x.toFloat()
-                        y = stimulus.new.y.toFloat()
-                        duration = 0.1f
-                    },
-                    Actions.run { done() }
+        game.gridActorRegistry.apply {
+            val comesIntoVision =
+                actorOf(stimulus.host) == null && tileIsVisible(stimulus.new)
+            val comesOutOfVision =
+                actorOf(stimulus.host) != null && !tileIsVisible(stimulus.new)
+            if (comesIntoVision) {
+                spawnRealThing(stimulus.host)
+                actorOf(stimulus.host)!!.setPosition(
+                    stimulus.old.x.toFloat(),
+                    stimulus.old.y.toFloat()
                 )
-            ) ?: done()
+            }
+            actorOf(stimulus.host)
+                ?.addAction(
+                    Actions.sequence(
+                        MoveToAction().apply {
+                            x = stimulus.new.x.toFloat()
+                            y = stimulus.new.y.toFloat()
+                            duration = 0.1f
+                        },
+                        Actions.run {
+                            if (comesOutOfVision) {
+                                removeActor(stimulus.host)
+                            }
+                            done()
+                        }
+                    )
+                )
+                ?: done()
+        }
     }
+
+    private fun tileIsVisible(voxel: Voxel) =
+        game.vicinity
+            .fieldOfView
+            .contains(voxel.x, voxel.y)
 
 }
